1269b07ab22edce2f0ee4f39f1d31eef7c5fe2e1
Kleysius <sebasti.thomass@gmail.com>
2023-04-03 13:54:54 +0200

    fifth version

--- model/Character.js
+++ model/Character.js
@@ -9,41 +9,36 @@ class Character {
         this.health = health;
         this.speed = speed;
         this.defense = defense;
+        this.boxShadow = "0 0 3px -1px transparent, 0 0 2px 1px transparent, 0 0 5px 0px transparent, 0px 10px 20px -5px black, 0 2px 15px -5px black, 0 0 20px 0px transparent";
         switch (this.classe) {
             case "Humain":
                 this.img = "url(./views/assets/img/humain.png)";
                 this.backgroundColor = "#4b782b61";
-                this.boxShadow = "0 0 20px #000";
                 this.border = "10px solid #1e2e12cc";
                 break;
             case "Ange":
                 this.img = "url(./views/assets/img/ange1.png)";
                 this.backgroundColor = "#f0f8ff63";
-                this.boxShadow = "0 0 20px #000";
                 this.border = "10px solid #4e5154c9";
                 break;
             case "Démon":
                 this.img = "url(./views/assets/img/demon3.png)";
                 this.backgroundColor = "#c53a4566";
-                this.boxShadow = "0 0 20px #000";
                 this.border = "10px solid #4a151ab5";
                 break;
             case "Mage":
                 this.img = "url(./views/assets/img/mage.png)";
                 this.backgroundColor = "#2e415280";
-                this.boxShadow = "0 0 20px #000";
                 this.border = "10px solid #162028d1";
                 break;
             case "Guerrier":
                 this.img = "url(./views/assets/img/guerrier.png)";
                 this.backgroundColor = "#9d90418a";
-                this.boxShadow = "0 0 20px #000";
                 this.border = "10px solid #4a441ec9";
                 break;
             case "Vampire":
                 this.img = "url(./views/assets/img/vampire2.png)";
                 this.backgroundColor = "#415f9d8a";
-                this.boxShadow = "0 0 20px #000";
                 this.border = "10px solid #262e46cf";
                 break;
             default:
